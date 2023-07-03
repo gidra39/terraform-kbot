68b73e11b51322d80502d24f7421df63ff5de972
gidra39 <gidra39@gmail.com>
2023-07-03 19:35:17 +0000

    try 1

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "gke_cluster" {
-  source         = "github.com/gidra39/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  source           = "github.com/gidra39/tf-google-gke-cluster"
+  GOOGLE_REGION    = var.GOOGLE_REGION
+  GOOGLE_PROJECT   = var.GOOGLE_PROJECT
+  GKE_NUM_NODES    = var.GKE_NUM_NODES
+  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,23 @@
 variable "GOOGLE_REGION" {
   description = "The Google Cloud region"
   type        = string
-  default     = "us-central1" 
+  default     = "europe-west1-d"
 }
 
 variable "GOOGLE_PROJECT" {
   description = "The Google Cloud project ID"
   type        = string
-  default     = "k8s-test5" // Замініть це значення на ID вашого проекту Google Cloud
+  default     = "k8s-test5"
 }
 
 variable "GKE_NUM_NODES" {
   description = "The number of nodes in the GKE cluster"
   type        = number
-  default     = 2 // Замініть це значення на потрібну кількість вузлів
+  default     = 1
 }
 
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "e2-small"
+  description = "Machine type"
+}
