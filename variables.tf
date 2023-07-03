55106a68465e3b30baa984a7f6715ae778bb1032
gidra39 <95176865+gidra39@users.noreply.github.com>
2023-07-03 10:34:14 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
+variable "GOOGLE_REGION" {
+  description = "The Google Cloud region"
+  type        = string
+  default     = "us-central1" 
+}
+
+variable "GOOGLE_PROJECT" {
+  description = "The Google Cloud project ID"
+  type        = string
+  default     = "k8s-test5" // Замініть це значення на ID вашого проекту Google Cloud
+}
+
+variable "GKE_NUM_NODES" {
+  description = "The number of nodes in the GKE cluster"
+  type        = number
+  default     = 2 // Замініть це значення на потрібну кількість вузлів
+}
 
